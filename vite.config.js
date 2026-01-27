7d6f67d07a1539bc9fc7a3799deb972b85a97331
joyprakashk <joyprakashk28@gmail.com>
2026-01-27 10:22:52 +0530

    chore: add deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',  //since deploying to joyprakashk.github.io
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
+  assetsInclude: ['**/*.glb'],  //maintain your existing GLB support
 })
